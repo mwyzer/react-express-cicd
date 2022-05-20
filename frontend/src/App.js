a96cc4cda81f65c37c8467ee16f706b1172add0a
mwyzer <muhammad.wyzer@gmail.com>
2022-05-20 14:11:01 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [userName, setUsername] = useState('')
+
+  useEffect(() => {
+    getNames();
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+    setUsername(response.data)
+  }
+
+  return (
+    <>
+      <h1>My Website</h1>
+      <h3>My Name is {userName}</h3>
+    </>
+
+  );
+}
+
+export default App;
